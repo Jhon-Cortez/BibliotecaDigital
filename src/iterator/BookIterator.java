5b696a83670dafba94ec51f91cd98a7d340b86ef
JuanP <jpchalaramirez@gmail.com>
2026-03-11 20:27:41 -0500

    feat(iterator): add BookIterator with hasNext and next methods to iterate over a book collection

---
+++ src/iterator/BookIterator.java
@@ -0,0 +1,28 @@
+package iterator;
+
+import java.util.List;
+
+public class BookIterator implements IIterator {
+    private List<Book> books;
+    private int position;
+
+    public BookIterator(List<Book> books) {
+        this.books = books;
+        this.position = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < books.getUser().size();
+    }
+
+    @Override
+    public Book next() {
+        if (hasNext()) {
+            return books.getUser().get(position++);
+        }
+        return null;
+    }
+
+    
+}
