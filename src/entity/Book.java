f8608052a58d27865ce00312f2c1582e8fb135b6
Jhon-Cortez <cortezvargasjhon01@gmail.com>
2026-03-12 17:34:10 -0500

    fix(Book):Add parametters and metohd

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -9,5 +9,59 @@ package entity;
  * @author rikyf
  */
 public class Book {
+    private long id;
+    private String title;
+    private String editorial;
+    private boolean status;
+    private String category;
+
+    public Book(long id, String title, String editorial, boolean status, String category) {
+        this.id = id;
+        this.title = title;
+        this.editorial = editorial;
+        this.status = status;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
     
+     
 }
