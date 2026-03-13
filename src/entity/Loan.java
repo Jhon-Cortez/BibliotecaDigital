bdae2931d5cc440109a3f31d48a09a0622e0b121
Jhon-Cortez <cortezvargasjhon01@gmail.com>
2026-03-12 23:13:27 -0500

    fix(loan):Add parametters and metohd

--- src/entity/Loan.java
+++ src/entity/Loan.java
@@ -8,6 +8,51 @@ package entity;
  *
  * @author rikyf
  */
+import java.util.Date;
+
 public class Loan {
+
+    private Long id;
+    private Date loanDate;
+    private Date returnDate;
+    private boolean returned;
+    private User user;
+    private Book book;
+
+    public Loan(User user, Book book) {
+        this.user = user;
+        this.book = book;
+        this.loanDate = new Date(); 
+        this.returned = false;
+    }
     
+    //Cada que se devuelve el libro guarda la hora y hace que el returned sea true
+    public void returnBook() {
+        this.returned = true;
+        this.returnDate = new Date();
+    }
+    
+    
+    // Obtiene la fecha en el que se presto
+    public Date getLoanDate() {
+        return loanDate;
+    }
+    
+    //Obtiene la fecha en que se devolvio
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    //Nos dice si est adecuelto o no
+    public boolean isReturned() {
+        return returned;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
 }
